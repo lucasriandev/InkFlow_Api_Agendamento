52c60971f7ec1e5433ee1169b86144078aa5ab5d
Lucas <lucasrian.dev@gmail.com>
2026-05-18 12:49:52 -0300

    Put, delete, get e post criados

--- backend/routes/portfolioRoutes.js
+++ backend/routes/portfolioRoutes.js
@@ -30,4 +30,37 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.put("/:id", async (req, res) => {
+  try {
+    //no express id se pega assim
+    const { id } = req.params;
+    const { estilo, descricao, imagemUrl } = req.body;
+
+    const trabalhoAtualizado = await prisma.trabalho.update({
+      where: { id: Number(id) }, // O ID vem como string da URL, temos de converter
+      data: { estilo, descricao, imagemUrl },
+    });
+
+    res.json({ mensagem: "Trabalho Atualizado", dados: trabalhoAtualizado });
+  } catch (error) {
+    res.status(500).json({
+      error: "Erro ao atualizar o trabalho. Verifique se o id existe.",
+    });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    await prisma.trabalho.delete({
+      where: { id: Number(id) },
+    });
+
+    res.json({ mensagem: "Trabalho apagado com sucesso!" });
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao apagar. Verifique se o Id existe" });
+  }
+});
+
 module.exports = router;
